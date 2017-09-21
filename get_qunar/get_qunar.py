2c6653baddc7b3e5875c3b444ecbff0dc720ddd5
Dreag <yang_g1@126.com>
2017-09-21 19:44:28 +0800

    get sight_link and info

--- get_qunar/get_qunar.py
+++ get_qunar/get_qunar.py
@@ -1,5 +1,39 @@
 # author:gaoyang
 # time:2017.9.21
+
 import requests
+import json
 from bs4 import BeautifulSoup
 
+cities = ["北京", "上海", "成都", "三亚", "广州", "重庆", "深圳", "西安", "杭州", "厦门", "武汉", "大连", "苏州", "青岛", "天津", "南京", "香港", "桂林",
+          "郑州", "昆明"]
+
+url = "http://piao.qunar.com/ticket/list.htm?from=mpshouye_theme&keyword=必游景点&region=北京&page=1"
+r = requests.get(url)
+
+r.raise_for_status()
+r.encoding = r.apparent_encoding
+
+
+html = r.text
+soup = BeautifulSoup(html,"html.parser")
+result_list = soup.find("div",{"class":"result_list"})
+
+# sight_item得到每一页景点链接所在标签
+sight_item = result_list.find_all("div",{"class":"sight_item_about"})
+indexlist = {}
+
+for l in sight_item[0:]:
+    information = []
+    title = l.find("a").get('title')
+    try:
+        level = l.find("span",{"class":"level"}).string
+    except:
+        level = "非A级景区"
+    information.append(level)
+    href = "piao.qunar.com" + l.find("a").get('href')  # 使用get获得标签中的属性内容
+    information.append(href)
+    indexlist[title] = information
+
+with open("sight_link.json","r+",encoding="utf-8") as f:
+    f.write(json.dumps(indexlist))
